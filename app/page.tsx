ebe7b304259810ba5c5bdab718c366e667c5aeb7
Gordon Liu <gordonliu1106@gmail.com>
2024-05-08 19:10:38 -0500

    chore: clean up unused components and paths

--- app/page.tsx
+++ app/page.tsx
@@ -12,44 +12,40 @@ const socials = [
 
 export default function Home() {
   return (
-    <div className="flex flex-row items-center justify-center overflow-hidden h-full">
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        <div className="flex flex-col items-start justify-center gap-6 p-24">
-          <div className="w-10 h-10 relative">
-            <Image
-              src={"/profile.jpg"}
-              alt={"Profile Picture"}
-              fill
-              className="object-cover rounded-full "
-            />
-          </div>
-          <h2 className="z-10 text-lg cursor-default font-medium space tracking-tight w-1/2">
-            Hi, I'm Gordon Liu. I'm a machine learning researcher focused on
-            agent interactions and code synthesis. My mission is to enable
-            individuals to bring their big ideas to life.
-          </h2>
-          <div className="flex flex-col items-start justify-center gap-4 text-sm font-light leading-5">
-            <h2>
-              Some of my interests include full-stack development, data science,
-              AI/ML, pure math, and startups. With a deep-rooted passion for
-              learning, I have always voraciously applied theory to solve real
-              world issues.
-            </h2>
-            <h2>Thanks for stopping by! Feel free to contact me above. ☺️</h2>
-          </div>
-          <div className="flex flex-row justify-between w-full border-b border-black">
-            <input className="bg-transparent w-full outline-none text-sm"></input>
-            <div>→</div>
-          </div>
-          <div>✌️</div>
-          <div className="relative top-16 left-8 flex flex-row w-full items-end justify-end gap-4">
-            {socials.map((item) => (
-              <a key={item.href} href={item.href} className="w-3 h-3">
-                {item.icon}
-              </a>
-            ))}
-          </div>
-        </div>
+    <div className="w-full h-full flex flex-col items-start justify-center gap-6 p-24">
+      <div className="w-10 h-10 relative">
+        <Image
+          src={"/profile.jpg"}
+          alt={"Profile Picture"}
+          fill
+          className="object-cover rounded-full "
+        />
+      </div>
+      <h2 className="z-10 text-lg cursor-default font-medium space tracking-tight w-1/2">
+        Hi, I'm Gordon Liu. I'm a machine learning researcher focused on agent
+        interactions and code synthesis. My mission is to enable individuals to
+        bring their big ideas to life.
+      </h2>
+      <div className="flex flex-col items-start justify-center gap-4 text-sm font-light leading-5">
+        <h2>
+          Some of my interests include full-stack development, data science,
+          AI/ML, pure math, and startups. With a deep-rooted passion for
+          learning, I have always voraciously applied theory to solve real world
+          issues.
+        </h2>
+        <h2>Thanks for stopping by! Feel free to contact me above. ☺️</h2>
+      </div>
+      <div className="flex flex-row justify-between w-full border-b border-black">
+        <input className="bg-transparent w-full outline-none text-sm"></input>
+        <div>→</div>
+      </div>
+      <div>✌️</div>
+      <div className="relative top-16 left-8 flex flex-row w-full items-end justify-end gap-4">
+        {socials.map((item) => (
+          <a key={item.href} href={item.href} className="w-3 h-3">
+            {item.icon}
+          </a>
+        ))}
       </div>
     </div>
   );
